29db901d551bcdebca941a82bed27db79f7a5f69
Tamar Cohen <tamar.e.cohen@nasa.gov>
2015-07-23 15:41:55 -0700

    tuned prep

--- manifests/site.pp
+++ manifests/site.pp
@@ -429,20 +429,11 @@ class site_setup {
     groups => ['www-data'],
   }
 
-# collectmedia is no longer used. prep calls collectstatic instead.
-#  exec { 'collectmedia':
-#    command => "${sitedir}/manage.py collectmedia",
-#    require => Exec['dbrestore'],
-#    user => $user,
-#  }
-
   exec { 'prep':
     # can't run this with root environment variables, because bower
     # crashes if it looks for files in root's home directory. setting the puppet
     # 'user' flag as shown below isn't sufficient. 'su' seems to be needed.
-    command => "/bin/su -l $user -c 'source ${sitedir}/sourceme.sh; ${sitedir}/manage.py prep'",
-    # notify =>{/bin/su -l $user -c 'echo $DJANGO_SETTINGS_MODULE'},
-    # user => $user,
+    command => "/bin/su -l $user -c '(cd gds/xgds_basalt && ./manage.py prep)'",
     require => Exec['bootstrap'],
   }
 
