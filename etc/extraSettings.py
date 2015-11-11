0b0195dc182bf2ccab295c493120663ca7013a65
Grace Lee <yeongrace.lee@gmail.com>
2015-11-11 15:13:04 -0800

    extraSettings.py changed

--- etc/extraSettings.py
+++ etc/extraSettings.py
@@ -2,13 +2,6 @@
 # <site>/management/templates/settings.py. They customize the setup for
 # a vagrant VM environment.
 
-from siteSettings import *  # pylint: disable=W0401
-
-# Make this unique, and don't share it with anybody.  Used by Django's
-# cookie-based authentication mechanism.
-#SECRET_KEY = 'f4-_mq%p-ipi^x)z*wcon%#ci#50(a1t&amp;+n@smwr)hx3_fg#-t'
-USING_APP_ENGINE = False
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.mysql',
