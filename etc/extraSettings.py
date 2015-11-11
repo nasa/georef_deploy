421d345779d04040528cce1bddf5ef19e7d1ac05
Grace Lee <yeongrace.lee@gmail.com>
2015-11-11 12:37:11 -0800

    scripts changed from xgds_basalt to georef

--- etc/extraSettings.py
+++ etc/extraSettings.py
@@ -2,10 +2,17 @@
 # <site>/management/templates/settings.py. They customize the setup for
 # a vagrant VM environment.
 
+from siteSettings import *  # pylint: disable=W0401
+
+# Make this unique, and don't share it with anybody.  Used by Django's
+# cookie-based authentication mechanism.
+#SECRET_KEY = 'f4-_mq%p-ipi^x)z*wcon%#ci#50(a1t&amp;+n@smwr)hx3_fg#-t'
+USING_APP_ENGINE = False
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.mysql',
-        'NAME': 'xgds_basalt',
+        'NAME': 'georef',
         'USER': 'vagrant',
         'PASSWORD': 'vagrant',
         'HOST': '127.0.0.1',
@@ -13,6 +20,6 @@ DATABASES = {
     }
 }
 
-SERVER_ROOT_URL = 'http://10.0.3.20/'
+SERVER_ROOT_URL = 'http://10.0.3.18/'
 
 GEOCAM_UTIL_SECURITY_ENABLED = False
